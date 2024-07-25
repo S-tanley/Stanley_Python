2eb4224b545320fd5c78e1c5b482c52fccf5e213
Zheng Bowen <2962786177@qq.com>
2024-07-25 11:53:09 +0800

    Update codetest3.py

--- codetest3.py
+++ codetest3.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def mymedian(self, myl):
+        # 根据定义算中位数
         length = len(myl)
         if length == 1:
             return myl[0]
@@ -11,22 +12,22 @@ class Solution(object):
             return res
 
     def mymean(self, myl):
+        # 平均数
         return sum(myl)/len(myl)
 
     def MaxWeigtVal(self, a, n):
         res = -10**9
-        all_subsequences = []
+        # 这里想的是直接遍历，每种子序列
+        # 有点问题因为复杂度这样的话是O（2**n*n）
         for i in range(2**n):
             subsequence = []
             for j in range(n):
-                # Check if j-th bit in i is set
                 if (i & (1 << j)) > 0:
                     subsequence.append(a[j])
             if(len(subsequence)== 0):
                 continue
             t = self.mymean(subsequence) - self.mymedian(subsequence)
             res = res if res > t else t
-            all_subsequences.append(subsequence)
         return res
     
 a = [1,3,5,9]
